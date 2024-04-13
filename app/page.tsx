68c0248257277e758af6bf5f2e14246e891e0edf
CoolBeans <147670938+cocoBEAMS@users.noreply.github.com>
2024-04-13 15:17:44 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section>
       <div className="h-screen flex flex-col items-center justify-center space-y-5">
-        <h1 className="inline-block bg-gradient-to-r from-blue-800 to-purple-800 text-transparent font-extrabold text-5xl bg-clip-text">3kh0 V5</h1>
+        <h1 className="inline-block bg-gradient-to-r from-blue-800 to-purple-800 text-transparent font-extrabold text-5xl bg-clip-text">PlayScape</h1>
         <p>Welcome to a new generation.</p>
         <Button>Play Now</Button>
       </div>
